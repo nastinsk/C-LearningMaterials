127d09a7a22df427b602a423446227f5787e5172
Anastasia <nastinsk@gmail.com>
2021-01-02 03:08:18 -0800

    divide an conauer task done

---
+++ AlgrithmsAndDS/DivideAndConquer/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DivideAndConquer
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        /*
+         * 1. Write a function called MultiplyList that takes in a List<int>. It should return the product of all numbers in the list. Solve this problem using iteration.
+         * 2. Write a unit test and test this function.
+         * 3. Solve the same problem using a recursive divide-and-conquer algorithm and test it again.
+         */
+        public static int MultiplyListIretaion(List<int> inputList)
+        {
+
+            int product=0;
+
+            if(inputList.Count > 0)
+            {
+                product = 1;
+                foreach (int el in inputList)
+                {
+                    product *= el;
+                }
+                return product;
+            }
+
+            return product;
+        }
+
+        public static int MultiplyListDivideAndConquer(List<int> inputList)
+        {
+        
+            if(inputList.Count == 0)
+            {
+                return 0;
+            }
+            if(inputList.Count == 1)
+            {
+                return inputList[0];
+            }
+            else
+            {
+                return inputList[0] * MultiplyListDivideAndConquer(inputList.GetRange(1, inputList.Count - 1));
+            }
+        }
+    }
+}
---
+++ AlgrithmsAndDS/DivideAndConquerTests/ProgramTests.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DivideAndConquer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DivideAndConquer.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void MultiplyListIretaionTest()
+        {
+            var inputList = new List<int> { 3, 2, 4 };
+            int expected = 24;
+
+            int actual = DivideAndConquer.Program.MultiplyListIretaion(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListIretaionTest1()
+        {
+            var inputList = new List<int> { 3, 2, 4, 0 };
+            int expected = 0;
+
+            int actual = DivideAndConquer.Program.MultiplyListIretaion(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListIretaionTest2()
+        {
+            var inputList = new List<int> { -3, 2, -4 };
+            int expected = 24;
+
+            int actual = DivideAndConquer.Program.MultiplyListIretaion(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListIretaionTest3()
+        {
+            var inputList = new List<int> { 1, 1, -1 };
+            int expected = -1;
+
+            int actual = DivideAndConquer.Program.MultiplyListIretaion(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListIretaionTest4()
+        {
+            var inputList = new List<int> { };
+            int expected = 0;
+
+            int actual = DivideAndConquer.Program.MultiplyListIretaion(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListDivideAndConquerTest()
+        {
+            var inputList = new List<int> { 3, 2, 4 };
+            int expected = 24;
+
+            int actual = DivideAndConquer.Program.MultiplyListDivideAndConquer(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListDivideAndConquerTest1()
+        {
+            var inputList = new List<int> { 3, 2, 4, 0 };
+            int expected = 0;
+
+            int actual = DivideAndConquer.Program.MultiplyListDivideAndConquer(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListDivideAndConquerTest2()
+        {
+            var inputList = new List<int> { -3, 2, -4 };
+            int expected = 24;
+
+            int actual = DivideAndConquer.Program.MultiplyListDivideAndConquer(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+        [TestMethod()]
+        public void MultiplyListDivideAndConquerTest3()
+        {
+            var inputList = new List<int> { 1, 1, -1 };
+            int expected = -1;
+
+            int actual = DivideAndConquer.Program.MultiplyListDivideAndConquer(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+
+        [TestMethod()]
+        public void MultiplyListDivideAndConquerTest4()
+        {
+            var inputList = new List<int> {};
+            int expected = 0;
+
+            int actual = DivideAndConquer.Program.MultiplyListDivideAndConquer(inputList);
+            Assert.AreEqual(expected, actual, "list product is not correct");
+        }
+
+    }
+}
