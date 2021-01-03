3c7a4ac6bfe2a86de78c78d7bbb0618ad1992aa4
Anastasia <nastinsk@gmail.com>
2021-01-02 16:51:28 -0800

    greedy algorithm task done

---
+++ AlgrithmsAndDS/Greedy/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Greedy
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+        /*
+         * 1. Write a function called FillRoomWithBoxes that takes in an int called roomSize, a List<int> called possibleSizes, and a List<int> called boxes.
+            * a. The possibleSizes list should contain all possible box sizes. You can assume this list is sorted in descending order.
+         * 2. The function should implement a greedy recursive algorithm to fill the room as full as it can get with the least number of boxes, and should store the size of each box used in the boxes list, one entry per box
+            * a. For example, if your possible sizes list contained 7, 3, 1, and your roomSize was 25, the boxes list should contain 7, 7, 7, 3, 1 when your function exits
+         * 3. Write a unit test and test this function
+         */
+
+        public static List<int> FillRoomWithBoxes(int roomSize, List<int> possibleSizes, List<int> boxes)
+        {
+            //take first element from possible Sizes list  from the roomsize value untill it is not < 0
+            //take second element if possible the same way
+            //repeat untill possible with all possiblesizes elements
+            if(possibleSizes.Count == 0)
+            {
+                return boxes;
+            }
+            if(roomSize - possibleSizes[0] < 0)
+            {
+                return FillRoomWithBoxes(roomSize, possibleSizes.GetRange(1, possibleSizes.Count - 1), boxes);
+            }
+            else
+            {
+                roomSize = roomSize - possibleSizes[0];
+                boxes.Add(possibleSizes[0]);
+                return FillRoomWithBoxes(roomSize, possibleSizes, boxes);
+            }
+
+
+            
+        }
+
+    }
+}
---
+++ AlgrithmsAndDS/GreedyTests/ProgramTests.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Greedy;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Greedy.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void FillRoomWithBoxesTest()
+        {
+            var boxSizes = new List<int> { 7, 3, 1 };
+            var boxes = new List<int> { };
+            var expected = new List<int> { 7, 7, 7, 3, 1 };
+            var actual = Greedy.Program.FillRoomWithBoxes(25, boxSizes, boxes);
+            Assert.AreEqual(expected[0], actual[0], "error in 1st element");
+            Assert.AreEqual(expected[1], actual[1], "error in 2nd element");
+            Assert.AreEqual(expected[2], actual[2], "error in 3rd element");
+            Assert.AreEqual(expected[3], actual[3], "error in 4th element");
+            Assert.AreEqual(expected[4], actual[4], "error in 5th element");
+        }
+
+        [TestMethod()]
+        public void CompareListsWithLinq()
+        {
+            var boxSizes = new List<int> { 7, 3, 1 };
+            var boxes = new List<int> { };
+            var expectedList = new List<int> { 7, 7, 7, 3, 1 };
+            var actualList = Greedy.Program.FillRoomWithBoxes(25, boxSizes, boxes);
+            bool expected = expectedList.SequenceEqual(actualList);
+            Assert.AreEqual(expected, true, "lists are not equal");
+        }
+
+        [TestMethod()]
+        public void CompareListsEmptyBoxSizes()
+        {
+            var boxSizes = new List<int> { };
+            var boxes = new List<int> { };
+            var expectedList = new List<int> { };
+            var actualList = Greedy.Program.FillRoomWithBoxes(25, boxSizes, boxes);
+            bool expected = expectedList.SequenceEqual(actualList);
+            Assert.AreEqual(expected, true, "lists are not equal");
+        }
+
+        [TestMethod()]
+        public void CompareListsZeroRoomSize()
+        {
+            var boxSizes = new List<int> {7, 3, 1 };
+            var boxes = new List<int> { };
+            var expectedList = new List<int> { };
+            var actualList = Greedy.Program.FillRoomWithBoxes(0, boxSizes, boxes);
+            bool expected = expectedList.SequenceEqual(actualList);
+            Assert.AreEqual(expected, true, "lists are not equal");
+        }
+
+        [TestMethod()]
+        public void CompareListsOnlyFristElUsed()
+        {
+            var boxSizes = new List<int> { 5, 3, 2 };
+            var boxes = new List<int> { };
+            var expectedList = new List<int> {5, 5};
+            var actualList = Greedy.Program.FillRoomWithBoxes(10, boxSizes, boxes);
+            bool expected = expectedList.SequenceEqual(actualList);
+            Assert.AreEqual(expected, true, "lists are not equal");
+        }
+
+        [TestMethod()]
+        public void CompareListsOnlyFristElUsed2()
+        {
+            var boxSizes = new List<int> { 5, 3, 2 };
+            var boxes = new List<int> { };
+            var expectedList = new List<int> { 5, 5 };
+            var actualList = Greedy.Program.FillRoomWithBoxes(11, boxSizes, boxes);
+            bool expected = expectedList.SequenceEqual(actualList);
+            Assert.AreEqual(expected, true, "lists are not equal");
+        }
+
+    }
+}
